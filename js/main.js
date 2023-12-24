170f0548452499d9abdd27cbbab5088092224401
lufwg <lufwg@proton.me>
2023-12-24 18:19:54 -0500

    feat: :sparkles: read the selected input
    we put the script tag again in the head, but this time we add an eventListener to the window load. we read what input of the named "Mokepon" group was cheked, wiht a forEach to the Nodelist.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,17 @@
-function chooseMokepon(){
-    alert("Good Choice");
+function runCode(){
+    let btnChooseMokepon = document.getElementById("BtnChooseMokepon");
+    btnChooseMokepon.addEventListener("click",chooseMokepon);
 };
-let btnChooseMokepon = document.getElementById("BtnChooseMokepon");
-btnChooseMokepon.addEventListener("click",chooseMokepon);
+let nodeList 
+function chooseMokepon(){
+    nodeList = document.getElementsByName("Mokepon");
+    /* console.log(nodeList); */
+    nodeList.forEach(input => {
+        /* console.log(input); */
+        if(input.checked){
+            /* console.log(input); */
+        }
+    });
+};
+
+window.addEventListener("load",runCode);
