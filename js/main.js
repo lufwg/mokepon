e22b80ed984572145d7eba70fc5914dc4f3bc672
lufwg <lufwg@proton.me>
2023-12-24 16:55:56 -0500

    feat: funtional Button
    we create a js file and his folder, and use addEventListeners to make the button btnChoiceMokepon work. HTML tag was forgotten, we added it. Script tag elemet is moved to the bottom  of the body tag.

---
+++ js/main.js
@@ -0,0 +1,5 @@
+function chooseMokepon(){
+    alert("Good Choice");
+};
+let btnChooseMokepon = document.getElementById("BtnChooseMokepon");
+btnChooseMokepon.addEventListener("click",chooseMokepon);
