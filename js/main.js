612af192a2c664231a996d2aa93f03436b07ab13
lufwg <lufwg@proton.me>
2023-12-25 12:25:00 -0500

    feat: :sparkles: Random Mokepon selected
    We add randomMokepon function that uses the old random function to obtain a random number  based on which we choose an index in the Mokepons array, then display it in the span html.

--- js/main.js
+++ js/main.js
@@ -3,12 +3,13 @@ function runCode(){
     btnChooseMokepon.addEventListener("click",chooseMokepon);
 };
 
-const $ = selector => document.getElementById(selector)
+const $ = selector => document.getElementById(selector);
 
 let nodeList;
 let mokeponChosen=0;
 let playerMokepon = $("PlayerMokepon");
 let pcMokepon = $("PcMokepon");
+
 function chooseMokepon(){
     nodeList = document.getElementsByName("Mokepon");
 /*     console.log(nodeList.length); */
@@ -17,11 +18,22 @@ function chooseMokepon(){
         if(input.checked){
             mokeponChosen++
             playerMokepon.innerHTML=input.id;
-        }
+        };
     });
     if(!mokeponChosen){
         alert("Choose A Mokepon To Continue");
-    }
+    }else{
+        randomMokepon();
+    };
+};
+
+function randomMokepon(){
+    let randomNumber = random(1,3);
+    let Mokepons=["Hipodoge","Capipepo","Ratigueya"];
+    pcMokepon.innerHTML=Mokepons[randomNumber];
+};
+    function random(min,max){
+    return Math.floor(Math.random()*(max-min+1)/* +min */);
 };
 
 window.addEventListener("load",runCode);
